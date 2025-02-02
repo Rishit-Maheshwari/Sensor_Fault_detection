44bd727589ac161af17626497f708a5a6566c1bc
Rishit-Maheshwari <rishitmaheshwari488@gmail.com>
2025-02-02 12:31:06 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import find_packages,setup
+from typing import List
+
+def get_requrirements(file_path:str)->list[str]:
+    requrirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
+    
+    # if HYPEN_E_DOT in requirements:
+    #     requirements.remove(HYPEN_E_DOT)
+    return requirements
+
+
+
+setup(
+    name="Fault Detection",
+    version="0.0.1",
+    author="Rishit",
+    author_email="rishitmaheshwari488@gmail.com",
+    install_requirements=get_requrirements("requirements.txt"),
+    packages=find_packages()
+)
